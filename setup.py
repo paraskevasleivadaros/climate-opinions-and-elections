b1dc7f9bb8dd15c49eec67f65fe74373e757e68e
pleivadaros <pleivadaros@digitalocean.com>
2025-01-19 12:50:08 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+# Load dependencies from requirements.txt
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setup(
+    name="climate-opinions-shift-elections-policies",  # Name of your package
+    version="0.1.0",  # Initial version
+    author="Paraskevas Leivadaros",
+    author_email="paraskevasleivadaros@gmail.com",
+    description="Exploring how attitudes toward climate change shift before and after elections, influenced by policies implemented during election cycles. Includes data analysis, visualizations, and models to uncover trends in public perception and the impact of political events on climate opinions",
+    long_description=open("README.md").read(),  # Use README.md as the long description
+    long_description_content_type="text/markdown",
+    url="https://github.com/paraskevasleivadaros/climate-opinions-shift-elections-policies",  # Link to your repository
+    packages=find_packages(),  # Automatically find packages in your project
+    install_requires=required,  # Use requirements.txt for dependencies
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',  # Minimum Python version
+)
